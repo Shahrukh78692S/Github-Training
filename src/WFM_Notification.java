fb08b55796aff1bc3c3b14bfe3b5c3e2f9a2a4ab
Shahrukh78692S <mdshahrukhsultan@gmail.com>
2025-08-03 16:15:14 +0530

    WFM Notification Class Created

--- src/WFM_Notification.java
+++ src/WFM_Notification.java
@@ -2,5 +2,7 @@ public class WFM_Notification {
     public static void main(String[] args) {
         System.out.println("I have learnt how to enable Git in IntelliJ");
         System.out.println("I have learnt how to add URL");
+        System.out.println("I have leart how to config user name and password");
+        System.out.println("I have learnt after that how to commit and push");
     }
 }
